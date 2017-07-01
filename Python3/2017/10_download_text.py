37bbc732996e33a49b526435c13c32f9c8639989
metalx1000 <github@filmsbykris.com>
2017-06-30 20:25:59 -0400

    added download scripts

---
+++ Python3/2017/10_download_text.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import urllib.request
+url = "https://pastebin.com/raw/ZuK9GmZT"
+response = urllib.request.urlopen(url)
+data = response.read()
+text = data.decode('utf-8')
+print(text)
---
+++ Python3/2017/11_download_files.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import urllib.request
+
+url = "https://i3.ytimg.com/vi/gq-jWftY0SU/hqdefault.jpg"
+file_name = "my_img.jpg" 
+urllib.request.urlretrieve(url, file_name)
